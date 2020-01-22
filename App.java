6488896ca32300b1e1775ad5ed345df29cf19863
bizimunda <yourbelover@gmail.com>
2020-01-23 00:05:53 +0100

    First commit

---
+++ App.java
@@ -0,0 +1,18 @@
+package be.bt.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	
+    	for (int i = 1; i <= 10; i++) {
+    		System.out.println(i+  " Hello World!" );
+			
+		}
+        
+    }
+}
---
+++ Hello.java
@@ -0,0 +1,7 @@
+package main.java.be.bt.java;
+
+public class Hello{
+    public static void main(String [] args){
+        System.out.println("Test");
+    }
+}
